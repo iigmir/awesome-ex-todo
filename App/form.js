311f2708850d355f5e2909892f543d76b527c757
iigmir <roc120j@gmail.com>
2022-12-22 14:41:48 +0800

    View to SafeAreaView

--- App/form.js
+++ App/form.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, TextInput, Button } from "react-native";
+import { SafeAreaView, TextInput, Button } from "react-native";
 import { styles } from "./style.js";
 
 export const TheForm = (props = { submit: () => {} }) => {
     const [text, onChangeText] = React.useState("Add todo...");
     const submit = () => props.submit(text);
-    return (<View>
+    return (<SafeAreaView>
         <TextInput style={styles.input} onChangeText={onChangeText} value={text} />
         <Button title="Create item" color="#f194ff" onPress={ submit } />
-    </View>);
+    </SafeAreaView>);
 };
