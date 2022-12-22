ead4c2458b6fd3170707250a7ddaeb89c1e72f24
iigmir <roc120j@gmail.com>
2022-12-22 14:42:28 +0800

    Text,

--- App/index.js
+++ App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { styles } from "./style.js";
 import { TheList } from "./list.js";
 import { TheForm } from "./form.js";
