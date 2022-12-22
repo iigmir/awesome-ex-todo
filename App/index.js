038988b186c25c970f098acd31817bfc03778d2c
iigmir <roc120j@gmail.com>
2022-12-22 11:41:04 +0800

    TheForm

--- App/index.js
+++ App/index.js
@@ -2,28 +2,32 @@ import React, { Component } from "react";
 import { Text, View, SafeAreaView, TextInput, Alert, Button } from "react-native";
 import { styles } from "./style.js";
 
-const TheForm = () => {
+const TheForm = (props = {
+    submit: () => {}
+}) => {
     const [text, onChangeText] = React.useState("Add todo...");
-    const submit = () => Alert.alert('Button with adjusted color pressed');
+    const submit = () => props.submit(text);
+    // Alert.alert('Button with adjusted color pressed');
     return (<SafeAreaView>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <Button
-            title="Press me"
-            color="#f194ff"
-            onPress={submit}
-        />
+        <TextInput style={styles.input} onChangeText={onChangeText} value={text} />
+        <Button title="Press me" color="#f194ff" onPress={ submit } />
     </SafeAreaView>);
 };
 
 export class MainApp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            list: [],
+        };
+    }
+    submitted(input) {
+        console.log(input);
+    }
     render() {
         return (<View style={ styles.container } className="the-container">
             <Text>Open up App.js to start working on your app!</Text>
-            <TheForm />
+            <TheForm submit={ () => this.submitted() } />
         </View>);
     }
 };
