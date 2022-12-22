9a3e13c72b6711f77096ba450c7fdbe760b93fbc
iigmir <roc120j@gmail.com>
2022-12-22 14:40:19 +0800

    View

--- App/index.js
+++ App/index.js
@@ -26,7 +26,7 @@ export const MainApp = () => {
         set_list(filter_list);
     };
     return (<View style={ styles.container }>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
         <TheForm submit={ submitted } />
         <TheList list={ list } remove={ remove_by_id } />
     </View>);
