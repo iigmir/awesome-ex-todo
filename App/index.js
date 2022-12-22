9c7f70a987535e8e515af48acc31901320441aeb
iigmir <roc120j@gmail.com>
2022-12-22 11:34:11 +0800

    Add TheForm

--- App/index.js
+++ App/index.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
-import { Text, View, StatusBar } from "react-native";
+import { Text, View, SafeAreaView, TextInput } from "react-native";
 import { styles } from "./style.js";
+
+const TheForm = () => {
+    const [text, onChangeText] = React.useState("Add todo...");
+    return (<SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+    </SafeAreaView>);
+};
 
 export class MainApp extends Component {
     render() {
-        return (<View style={styles.container} className="the-container">
+        return (<View style={ styles.container } className="the-container">
             <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <TheForm />
         </View>);
     }
 };
-
-export const Cat = () => {
-    return <Text>Hello, I am your cat!</Text>;
-};
-
--- App/style.js
+++ App/style.js
@@ -7,4 +7,10 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
 });
